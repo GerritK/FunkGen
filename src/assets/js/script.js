4606605c2ae22341ab19a5da4dd6d7f8d0a231ee
Gerrit Kaul <gerrit.kaul@ucware.de>
2016-11-01 16:43:47 +0100

    added multiple message destinations

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -32,15 +32,15 @@
 
     var vehicles = [
         {'name': 'TSF-W MA', 'number': 'Florian Braunschweig 9-41-43'},
-        {'name': 'TSF-W GF', 'number': 'Florian Braunschweig 9-41-43'},
-        {'name': 'TSF-W AT', 'number': 'Florian Braunschweig 9-41-43'},
-        {'name': 'TSF-W WT', 'number': 'Florian Braunschweig 9-41-43'},
-        {'name': 'TSF-W ST', 'number': 'Florian Braunschweig 9-41-43'}
+        {'name': 'TSF-W GF', 'number': 'Florian Braunschweig 9-41-44'},
+        {'name': 'TSF-W AT', 'number': 'Florian Braunschweig 9-41-45'},
+        {'name': 'TSF-W WT', 'number': 'Florian Braunschweig 9-41-46'},
+        {'name': 'TSF-W ST', 'number': 'Florian Braunschweig 9-41-47'}
     ];
 
     var messages = [
-        {'src': 0, 'dst': 1, 'announcement': false, msg: 'Hello World!', offset: 5},
-        {'src': 2, 'dst': 4, 'announcement': true, msg: 'Hello Earth!', offset: 7},
-        {'src': 1, 'dst': 3, 'announcement': false, msg: 'Hello Moon!', offset: 10}
+        {'src': 0, 'dst': [1], 'announcement': false, msg: 'Hello World!', offset: 5},
+        {'src': 2, 'dst': [4], 'announcement': true, msg: 'Hello Earth!', offset: 7},
+        {'src': 1, 'dst': [3,2], 'announcement': false, msg: 'Hello Moon!', offset: 10}
     ];
 })();
